e729b57fe54e57f534cbda215c0700f1db2bb8b4
joelsoto <sotojoel193@gmail.com>
2022-07-19 16:18:03 -0500

    crud

--- src/main/java/ec/edu/insteclrg/persistence/CiudadRepository.java
+++ src/main/java/ec/edu/insteclrg/persistence/CiudadRepository.java
@@ -3,7 +3,6 @@ package ec.edu.insteclrg.persistence;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import ec.edu.insteclrg.domain.Ciudad;
-import ec.edu.insteclrg.domain.Provincia;
 
 	public interface CiudadRepository extends JpaRepository<Ciudad, Long>{
 		public Ciudad findByNombre(String nombre);
